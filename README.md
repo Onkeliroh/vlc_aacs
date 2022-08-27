412357130865a88fed247201df58e7bee16c6d27
Onkeliroh <danielpollack@posteo.de>
2022-08-27 20:08:14 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# ABOUT
+
+This repository amis to ease the process of installing the **aacs** codec for VLC 3.x.
+
+The knowledge and resources are provided by https://vlc-bluray.whoknowsmy.name/
---
+++ install.ps1
@@ -0,0 +1,46 @@
+$ErrorActionPreference = "Stop"
+$downloadFolderPath = "$HOME\Downloads"
+$lineSeparator = "----------------------------------"
+
+
+Write-Host "Starting Installation" -ForegroundColor Yellow -BackgroundColor Black
+Write-Host $lineSeparator
+
+$aacsKeyDbArchiveName = "aacs_keydb.tar.bz2"
+$aacsKeyDbFolder = "C:\ProgramData\aacs"
+$keyDbFileName = "keydb.cfg"
+Write-Host "Downloading aacs_keydb file"
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("http://fvonline-db.bplaced.net/fv_download.php?lang=eng", "$downloadFolderPath\$aacsKeyDbArchiveName")
+
+Write-Host "Download done"
+
+Write-Host "Unpacking aacs_keydb archive"
+# x Extract, v Verbose, j bzip2, f Location
+tar -xvjf $downloadFolderPath\$aacsKeyDbArchiveName -C $downloadFolderPath
+
+Remove-Item -Path $downloadFolderPath\$aacsKeyDbArchiveName
+Write-Host "Moving aacs_keydb file"
+
+if (!(Test-Path -Path $aacsKeyDbFolder)) {
+    New-Item -Path $aacsKeyDbFolder -ItemType Directory
+}
+Move-Item -Path $downloadFolderPath\$keyDbFileName -Destination $aacsKeyDbFolder\$keyDbFileName -Force
+
+Write-Host "Installing codec library" -ForegroundColor Yellow -BackgroundColor Black
+$libaacsFileName = "libaacs.dll"
+
+$WebClient.DownloadFile("https://vlc-bluray.whoknowsmy.name/files/win64/libaacs.dll", "$downloadFolderPath\$libaacsFileName")
+
+Move-Item -Path $downloadFolderPath\$libaacsFileName -Destination 'C:\Program Files\VideoLAN\VLC\$libaacsFileName'
+
+Write-Host $lineSeparator
+if ($Error) {
+    Write-Host "Installation failed" -ForegroundColor Red -BackgroundColor Black
+    Write-Error $_.Exception.GetType().FullName
+    Write-Host $_.Exception.Message
+    return
+}
+Write-Host "Installation done" -ForegroundColor Green -BackgroundColor Black
+Write-Host "Next steps: Start VLC and enjoy your BluRay"
